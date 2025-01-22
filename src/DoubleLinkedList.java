5df237b3c28459b4dd4c7465b4d172eb5413a93b
ahmed <amohned346@gmail.com>
2025-01-22 18:57:52 +0300

    init commit

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -1,3 +1,83 @@
-public class DoubleLinkedList {
+public class DoubleLinkedList<E> {
+    //---------------- nested Node class ----------------
+    private static class Node<E> {
+        private E element;          // reference to the element stored at this node
+        private Node<E> prev;       // reference to the previous node in the list
+        private Node<E> next;       // reference to the subsequent node in the list
 
+        public Node(E e, Node<E> p, Node<E> n) {
+            element = e;
+            prev = p;
+            next = n;
+        }
+
+        public E getElement() { return element; }
+        public Node<E> getPrev() { return prev; }
+        public Node<E> getNext() { return next; }
+        public void setPrev(Node<E> p) { prev = p; }
+        public void setNext(Node<E> n) { next = n; }
+    } //----------- end of nested Node class -----------
+
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
+
+    /** Constructs a new empty list. */
+    public DoubleLinkedList() {
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null, header, null);
+        header.setNext(trailer);
+    }
+
+    public int size() { return size; }
+
+    /** Tests whether the linked list is empty. */
+    public boolean isEmpty() { return size == 0; }
+
+    public E first() {
+        if (isEmpty()) return null;
+        return header.getNext().getElement();
+    }
+
+    public E last() {
+        if (isEmpty()) return null;
+        return trailer.getPrev().getElement();
+    }
+
+    public void addFirst(E e) {
+        addBetween(e, header, header.getNext());
+    }
+
+    public void addLast(E e) {
+        addBetween(e, trailer.getPrev(), trailer);
+    }
+
+    public E removeFirst() {
+        if (isEmpty()) return null;
+        return remove(header.getNext());
+    }
+
+
+    public E removeLast() {
+        if (isEmpty()) return null; // nothing to remove
+        return remove(trailer.getPrev()); // last element is before trailer
+    }
+
+    // private update methods
+    private void addBetween(E e, Node<E> predecessor, Node<E> successor) {
+        // create and link a new node
+        Node<E> newest = new Node<>(e, predecessor, successor);
+        predecessor.setNext(newest);
+        successor.setPrev(newest);
+        size++;
+    }
+
+    private E remove(Node<E> node) {
+        Node<E> predecessor = node.getPrev();
+        Node<E> successor = node.getNext();
+        predecessor.setNext(successor);
+        successor.setPrev(predecessor);
+        size--;
+        return node.getElement();
+    }
 }
